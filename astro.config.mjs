eb6f8500ed1578f4d84597ca6494e3ad702d1965
Cédric Bontems <67456035+cbontems@users.noreply.github.com>
2023-09-14 16:59:58 +0200

    update site config to netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://rythm.nation",
+  site: "https://main--cute-travesseiro-0e201a.netlify.app/",
 });
